62c3e610f83fe92091bbe2eddb23c03d427ff73a
ALEX CHEUNG (INFRA-ISD-OOCLL/HKG) <CHEUNAL@OOCL.COM>
2018-11-21 14:34:48 +0800

    for loop version

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,22 @@
 'use strict';
 
 function choose_even(collection) {
-
+  let result = filter(collection,isEven);
+  return result
   //implement here
+
 }
 
+function filter(collection, fn){
+  const result = []
+  for(let i= 0;i< collection.length;i++){
+    if(fn(collection[i]))
+      result.push(collection[i])
+  }
+  return result
+}
+
+function isEven(element){
+  return (element%2===0)
+}
 module.exports = choose_even;
