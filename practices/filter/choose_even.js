38aaae7cc8d2ed5c90f07d6b22a4924428b8c19d
ALEX CHEUNG (INFRA-ISD-OOCLL/HKG) <CHEUNAL@OOCL.COM>
2018-11-21 14:48:25 +0800

    revised choose even

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,22 +1,10 @@
 'use strict';
 
 function choose_even(collection) {
-  let result = filter(collection,isEven);
-  return result
+
+  return collection.filter(a => a %2===0)
   //implement here
 
 }
 
-function filter(collection, fn){
-  const result = []
-  for(let i= 0;i< collection.length;i++){
-    if(fn(collection[i]))
-      result.push(collection[i])
-  }
-  return result
-}
-
-function isEven(element){
-  return (element%2===0)
-}
 module.exports = choose_even;
